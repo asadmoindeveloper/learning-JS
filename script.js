e874b2ad16fa326281418bed4b0d9bc608a47316
Asad Moin <asadmoin4469@gmail.com>
2025-11-30 20:55:06 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+/*
+used to print
+
+console.log("here is my first javascript code")
+
+*/
+
+/*
+variables and constants
+
+const stdId = 12345
+let Name = "Asad"
+
+console.log(stdId);
+console.log(Name);
+
+*/
+
+/*
+
+Data types of Java script
+
+number
+bigint
+string
+boolean 
+null
+undefined
+symbol
+
+
+console.log(typeof "asad");
+*/
+
+/*
+Conversions
+String to number:
+
+let score = "33abc"
+let convertscore = Number(score)
+console.log(convertscore);
+console.log(typeof convertscore);
+
+from number to boolean:
+
+let answer = 1
+let convertanswer = Boolean(answer)
+console.log(convertanswer);
+
+*/
+
+// premitive data types : boolean, number, string, symbol, null, undefined, BigInt
+
+// non premitive or reference data type: Array, Objects, Functions
+
+// This is how Array defined: let cars = [BMW, Mercedes, Audio, MarkX]
+
+/*
+this is how we make functions
+
+let carfunctions = function(){
+    console.log("hello world");
+    
+}
+
+*/
+
+
+/*
+this is how objects are made
+
+let obj = {
+    Carname: MarkX,
+    Engine: v8
+}
+*/
+
+/*
+let name = "        asad    "
+console.log(name.trim());
+
+there are two more methods that are trimstart and trimend
+*/
+
+// there are many different functions for the string data type you can explore on mdn (.lowercase, .uppercase, .indexof, .length)
+
+
